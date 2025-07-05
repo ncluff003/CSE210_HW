ff7fe1c81f968abced364a2d38b2817aa6cee43f
ncluff003 <ncluff003@zohomail.com>
2025-07-04 22:28:47 -0600

    Completed exercise 2 of week 1 along with the stretch challenge of adding signs to the letter grade.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        float grade = float.Parse(Console.ReadLine());
+        string letter;
+        string sign = "";
+        float gradeRemainder = grade % 10;
+
+        if (gradeRemainder >= 7)
+        {
+            sign = "+";
+        }
+        else if (gradeRemainder < 3)
+        {
+            sign = "-";
+        }
+
+        if (grade >= 90)
+        {
+            if (sign == "-")
+            {
+                letter = $"A{sign}";
+            }
+            else
+            {
+                letter = "A";
+            }
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = $"B{sign}";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = $"C{sign}";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = $"D{sign}";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
