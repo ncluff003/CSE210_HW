16f21932f3ed67cd325f23615348863f42bead4b
ncluff003 <ncluff003@zohomail.com>
2025-07-04 23:31:04 -0600

    Completed exercise 4 for week 1, including all of the stretch goals.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<float> numbers = new List<float>();
+        float userNumber = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+
+        do
+        {
+            userNumber = float.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber != 0);
+
+        float sum = 0;
+        float maximum = 0;
+        float minimum = 999999999999999999;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (maximum < number)
+            {
+                maximum = number;
+            }
+            if (minimum > number && number > 0)
+            {
+                minimum = number;
+            }
+        }
+        float average = sum / numbers.Count;
+
+        numbers.Sort();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maximum}");
+        Console.WriteLine($"The smallest positive number is: {minimum}");
+        Console.WriteLine($"The sorted list is: {string.Join(", ", numbers)}");
     }
 }
