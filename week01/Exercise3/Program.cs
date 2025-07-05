d734a6ac606e459e868ba6f7fe66443cf07874a0
ncluff003 <ncluff003@zohomail.com>
2025-07-04 22:54:57 -0600

    Completed exercise 3 of week 1, including the stretch challenges of tracking guesses and making it possible to keep playing.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool answer = true;
+        do
+        {
+            int guesses = 0;
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1, 101);
+            int magicNumber = randomNumber;
+            int guess = -1;
+            do
+            {
+                Console.WriteLine("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guesses++;
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You got it in {guesses} guesses! Great job!");
+                }
+            } while (guess != magicNumber);
+
+            Console.WriteLine("Do you want to play again? (e.g. yes / no)");
+            string response = Console.ReadLine();
+
+            if (response == "yes")
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+        } while (answer == true);
     }
 }
