b6b7e188fa78a1c2d4e52db0fb1fce8c72b2aa2c
ncluff003 <ncluff003@zohomail.com>
2025-07-12 22:21:57 -0600

    Stubbed Classes For Journal Project

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Journal
+{
+  public List<Entry> _entries;
+
+  public void StartEntry() { }
+  public void AddEntry(Entry newEntry) {
+
+  }
+  public void SaveToFile(string file)
+  {
+    
+  }
+  public void LoadFromFile(string file)
+  { 
+    
+  }
+  public void DisplayAll() {
+
+  }
+  
+}
---
+++ week02/Journal/Menu.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Menu
+{
+  public object _menu;
+  public void DisplayMenu() { }
+}
