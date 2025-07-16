1088cad2c39b02555119aa97cea11c13657fcbc7
ncluff003 <ncluff003@zohomail.com>
2025-07-16 07:09:27 -0600

    Implemented a loop to repeatedly show a menu until the user selects option 5 to quit.

--- week02/Journal/Menu.cs
+++ week02/Journal/Menu.cs
@@ -2,6 +2,22 @@ using System;
 
 public class Menu
 {
-  public object _menu;
-  public void DisplayMenu() { }
+  public List<string> _menu = new List<string>
+  {
+    "Please select one of the following choices:",
+    "---------------------------------",
+    "1. Write",
+    "2. Display",
+    "3. Load",
+    "4. Save",
+    "5. Quit",
+    "What would you like to do? (1-5)"
+  };
+  public void DisplayMenu()
+  {
+    foreach (string item in _menu)
+    {
+      Console.WriteLine(item);
+    }
+  }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
+        
+        Menu menu = new Menu();
+        menu.DisplayMenu();
+        int answer = int.Parse(Console.ReadLine());
+
+        while (answer != 5)
+        {
+
+            // Now this is where I need to add functionality to whatever the user selects in the menu.
+            // 1. Write -- Writing a new entry.
+            // 2. Display -- Displaying all of the journal entries.
+            // 3. Load -- Load the journal entries from a file.
+            // 4. Save -- Save the journal to a file.
+            // 5. Quit -- Exiting the journal program.
+
+            menu = new Menu();
+            menu.DisplayMenu();
+            answer = int.Parse(Console.ReadLine());
+        
+        }
+
         Console.WriteLine("Hello World! This is the Journal Project.");
     }
 }
