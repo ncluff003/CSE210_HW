1d9bf0e5cc30aa2c46eaa669b4732d9860a23cf3
ncluff003 <ncluff003@zohomail.com>
2025-07-16 07:29:45 -0600

    Implemented the prompt generator, including adding five more options of my own.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -11,20 +11,34 @@ class Program
 
         while (answer != 5)
         {
-
-            // Now this is where I need to add functionality to whatever the user selects in the menu.
-            // 1. Write -- Writing a new entry.
-            // 2. Display -- Displaying all of the journal entries.
-            // 3. Load -- Load the journal entries from a file.
-            // 4. Save -- Save the journal to a file.
-            // 5. Quit -- Exiting the journal program.
+            switch (answer)
+            {
+                // Now this is where I need to add functionality to whatever the user selects in the menu.
+                // 1. Write -- Writing a new entry.
+                case 1:
+                    break;
+                // 2. Display -- Displaying all of the journal entries.
+                case 2:
+                    break;
+                // 3. Load -- Load the journal entries from a file.
+                case 3:
+                    break;
+                // 4. Save -- Save the journal to a file.
+                case 4:
+                    break;
+                // 5. Quit -- Exiting the journal program.
+                case 5:
+                    Console.WriteLine("Exiting the Journal Program. Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid selection. Please choose a valid option.");
+                    break;
+            }
 
             menu = new Menu();
             menu.DisplayMenu();
             answer = int.Parse(Console.ReadLine());
         
         }
-
-        Console.WriteLine("Hello World! This is the Journal Project.");
     }
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -3,9 +3,27 @@ using System;
 public class PromptGenerator
 {
 
-  public List<string> _prompts;
+  public List<string> _prompts = new List<string>
+  {
+    "Who was the most interesting person I interacted with today?",
+    "What have I learned today",
+    "What was the best part of my day?",
+    "What was the most challenging aspect of my day?",
+    "How did I see the hand of the Lord in my life today?",
+    "What would be one way I could improve my day tomorrow?",
+    "What was the strongest emotion I felt today?",
+    "Who could I serve or help tomorrow, and how?",
+    "If I had one thing I could do over today, what would it be?",
+    "How do I feel the Savior would feel about my actions today?"
+  };
   public string GetRandomPrompt()
   {
-    return "";
+    Random random = new Random();
+
+    int index = random.Next(_prompts.Count);
+    
+    string prompt = _prompts[index];
+
+    return prompt;
   }
 }
